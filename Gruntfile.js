e519afbd7da1e91ecf6feb0675ffa76ae35dabab
Henrique Vicente <henriquevicente@gmail.com>
2013-09-24 02:18:48 -0300

    Add missing test to the yeomanConfig object
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,8 @@ module.exports = function (grunt) {
     // configurable paths
     var yeomanConfig = {
         app: 'app',
-        dist: 'dist'
+        dist: 'dist',
+        test: 'test'
     };
 
     try {
