4a63afc3fd0035e936fe72b4fbb3db2b0866937e
Henrique Vicente <henriquevicente@gmail.com>
2013-10-02 17:37:33 -0300

    Adding angular.js app config file
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ app/config.js
@@ -0,0 +1,12 @@
+/*global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('config', [])
+        .constant('config', {
+            facebook: {
+                id: '699869570038149'
+            }
+        });
+}());
