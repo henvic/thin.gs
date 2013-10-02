ccbfd0c5b6817b1f22ad0a0ba8c41fc9d3a2cd42
Henrique Vicente <henriquevicente@gmail.com>
2013-10-01 23:47:42 -0300

    Remove a example code
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,10 +4,4 @@
 angular.module('thin.gsApp')
     .controller('MainCtrl', function ($scope) {
         'use strict';
-
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
     });
