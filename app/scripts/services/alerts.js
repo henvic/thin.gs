e21e9494a6f708cc245c94e6e6d7e3af7c8f10e6
Henrique Vicente <henriquevicente@gmail.com>
2013-10-02 00:47:39 -0300

    Adding the alerts service
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ app/scripts/services/alerts.js
@@ -0,0 +1,32 @@
+/*global angular */
+/*jslint browser: true */
+(function () {
+    'use strict';
+
+    angular.module('alerts', [])
+        .factory('alert', function ($rootScope, $timeout) {
+            return {
+                add: function (msg) {
+                    $timeout(function () {
+                        $rootScope.$broadcast('alertNewMessage', msg);
+                    }, 300);
+                }
+            };
+        })
+        .controller('AlertsCtrl', function ($sanitize, $scope, $rootScope) {
+            $scope.alerts = [
+            ];
+
+            $scope.alerts.add = function (msg) {
+                $scope.alerts.push({raw: $sanitize(msg)});
+            };
+
+            $scope.alerts.remove = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
+
+            $rootScope.$on('alertNewMessage', function (e, msg) {
+                $scope.alerts.add(msg);
+            });
+        });
+}());
