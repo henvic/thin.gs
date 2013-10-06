e86ad8470b1f6e09b006a7c395182466c21a6518
Henrique Vicente <henriquevicente@gmail.com>
2013-10-06 04:54:18 -0300

    Cache the adapter collection and remove the overkill adapter.get() test
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- service/lib/users.js
+++ service/lib/users.js
@@ -3,20 +3,14 @@
 module.exports = function (adapter) {
     'use strict';
 
-    exports.get = function (id, callback) {
-        if (!adapter.get()) {
-            console.error('Failed to load DB');
-        }
+    var collection = adapter.get().collection('users');
 
-        adapter.get().collection('users').findOne({id: id}, callback);
+    exports.get = function (id, callback) {
+        collection.findOne({id: id}, callback);
     };
 
     exports.save = function (id, data) {
-        if (!adapter.get()) {
-            console.error('Failed to load DB');
-        }
-
-        adapter.get().collection('users').update({id: id}, data, {upsert: true}, function (err) {
+        collection.update({id: id}, data, {upsert: true}, function (err) {
             if (err) {
                 console.error(err);
             }
