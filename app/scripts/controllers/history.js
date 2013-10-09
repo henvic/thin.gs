deb44d8932edf86a8f925f9aefa71c8783bb256d
Henrique Vicente <henriquevicente@gmail.com>
2013-10-09 00:58:43 -0300

    Adding the basic history controller
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ app/scripts/controllers/history.js
@@ -0,0 +1,46 @@
+/*global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('thin.gsApp')
+        .factory('listItems', function ($resource) {
+            return $resource(
+                '/api/history/:id',
+                {id: '@id'}
+            );
+        })
+        .controller('HistoryCtrl', function ($scope, alert, listItems, moment) {
+            // /history should load only for logged in users
+            alert.add('Loading history twice, why? is that it?');
+            $scope.list = listItems.query();
+            $scope.itemDate = moment().format('YYYY-MM-DD');
+
+            $scope.addNewItem = function () {
+                listItems.save({
+                    'date': $scope.itemDate,
+                    'place': $scope.itemPlace
+                }).$promise.then(
+                    function (data) {
+                        $scope.list.push(data);
+                        $scope.itemDate = moment().format('YYYY-MM-DD');
+                        $scope.itemPlace = '';
+                    },
+                    function () {
+                        alert.add('Error adding item to the history records.');
+                    }
+                );
+            };
+
+            $scope.deleteItem = function (item) {
+                item.$remove().then(
+                    function () {
+                        $scope.list.splice($scope.list.indexOf(item), 1);
+                    },
+                    function () {
+                        alert.add('Error removing item from the history records.');
+                    }
+                );
+            };
+        });
+}());
