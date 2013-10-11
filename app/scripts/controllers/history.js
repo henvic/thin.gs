20d66592ba2cbefb085ab453d5b471cfc4a3bc56
Henrique Vicente <henriquevicente@gmail.com>
2013-10-11 14:51:37 -0300

    Refactoring the history
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -3,6 +3,22 @@
 (function () {
     'use strict';
 
+    var calculateAge = function (birthDate, otherDate) {
+        var years;
+
+        birthDate = new Date(birthDate || new Date());
+        otherDate = new Date(otherDate || new Date());
+
+        years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+        if (otherDate.getMonth() < birthDate.getMonth() ||
+                (otherDate.getMonth() === birthDate.getMonth() && otherDate.getDate() < birthDate.getDate())) {
+            years -= 1;
+        }
+
+        return years;
+    };
+
     angular.module('thin.gsApp')
         .factory('listItems', function ($resource) {
             return $resource(
@@ -10,22 +26,66 @@
                 {id: '@id'}
             );
         })
-        .controller('HistoryCtrl', function ($scope, alert, listItems, moment) {
+        .factory('centers', function ($resource) {
+            return $resource('/api/centers.json');
+        })
+        .directive('nextDonation', function (moment, $filter, userService) {
+            var orderBy = $filter('orderBy'),
+                birthday = userService.get().facebook.birthday,
+                age = calculateAge(birthday),
+                minAfter = (age < 60) ? 90 : 180;
+
+            return {
+                restrict : 'A',
+                templateUrl: 'views/donation/info.html',
+                link: function (scope, elem, attrs) {
+                    scope.age = age;
+                    scope.$watch("list", function () {
+                        var last = orderBy(scope.list, '-date')[0],
+                            minDate,
+                            d;
+
+                        if (last && last.date) {
+                            minDate = last.date;
+                        }
+
+                        d = moment(minDate, "YYYY-MM-DD").add('days', minAfter);
+
+
+                        if (d < moment()) {
+                            scope.waitToDonate = false;
+                            return;
+                        }
+
+                        scope.waitToDonate = d.fromNow() + ' (' + d.calendar() + ')';
+                    }, true);
+                }
+            };
+        })
+        .controller('HistoryCtrl', function ($scope, alert, listItems, centers, moment) {
             $scope.list = listItems.query();
-            $scope.itemDate = moment().format('YYYY-MM-DD');
+
+            $scope.centers = centers.query();
+
+            $scope.newItemDate = moment().format('YYYY-MM-DD');
+            $scope.end = true;
 
             $scope.addNewItem = function () {
+                $scope.end = false;
+
                 listItems.save({
-                    'date': $scope.itemDate,
-                    'place': $scope.itemPlace
-                }).$promise.then(
-                    function (data) {
-                        $scope.list.push(data);
-                        $scope.itemDate = moment().format('YYYY-MM-DD');
-                        $scope.itemPlace = '';
-                    },
+                    date: $scope.newItemDate,
+                    place: $scope.newItemPlace
+                }).$promise.then(function (data) {
+                    $scope.list.push(data);
+                    $scope.newItemDate = moment().format('YYYY-MM-DD');
+                    $scope.newItemPlace = '';
+                },
                     function () {
                         alert.add('Error adding item to the history records.');
+                    }).then(
+                    function () {
+                        $scope.end = true;
                     }
                 );
             };
