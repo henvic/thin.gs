ab7cebd5e5791248cd30f18618467b608f5f4d21
Henrique Vicente <henriquevicente@gmail.com>
2013-10-11 15:09:38 -0300

    Adding the user preferences
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,32 @@
     'use strict';
 
     angular.module('thin.gsApp')
-        .controller('MainCtrl', function ($scope) {
+        .controller('MainCtrl', function ($scope, userService, alert) {
+            $scope.bloods = [
+                {value: 'A', text: 'A'},
+                {value: 'B', text: 'B'},
+                {value: 'AB', text: 'AB'},
+                {value: 'O', text: 'O'},
+                {value: '', text: 'não definido'}
+            ];
+
+            $scope.rhs = [
+                {value: '-', text: '-'},
+                {value: '+', text: '+'},
+                {value: '', text: 'não definido'}
+            ];
+
+            $scope.set = function () {
+                var data = {
+                    blood: $scope.user.blood,
+                    notifications: $scope.user.notifications
+                };
+
+                userService.save(data, function (err) {
+                    if (err) {
+                        alert.add('Error setting your preferences. We advise you to reload this application.');
+                    }
+                });
+            };
         });
 }());
