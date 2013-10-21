4c135a4c8a4af85feff866dbefdbd594e8274a33
Henrique Vicente <henriquevicente@gmail.com>
2013-10-21 11:33:04 -0300

    Source formatting / making DI explicit
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -7,57 +7,58 @@
         .config(['FacebookProvider', 'config', function (FacebookProvider, config) {
             FacebookProvider.init(config.facebook.id);
         }])
-        .factory('userService', function ($http, $window, $q, $rootScope, alert) {
-            var set,
-                defer = $q.defer(),
-                end = true;
+        .factory('userService', ['$http', '$window', '$q', '$rootScope', 'alert',
+            function ($http, $window, $q, $rootScope, alert) {
+                var set,
+                    defer = $q.defer(),
+                    end = true;
 
-            set = function (user) {
-                $rootScope.user = user;
-            };
+                set = function (user) {
+                    $rootScope.user = user;
+                };
 
-            (function () {
-                $http({ method: 'GET', url: '/api/me' }).then(
-                    function (user) {
-                        set(user.data);
+                (function () {
+                    $http({ method: 'GET', url: '/api/me' }).then(
+                        function (user) {
+                            set(user.data);
+                        },
+                        function () {
+                            alert.add('Error in retrieving user data.');
+                        }
+                    );
+                }());
+
+                return {
+                    get: function () {
+                        return $rootScope.user;
                     },
-                    function () {
-                        alert.add('Error in retrieving user data.');
-                    }
-                );
-            }());
+                    login: function () {
+                        $window.location.href = '/auth/facebook';
+                    },
+                    logout: function () {
+                        $http({ method: 'POST', url: '/logout' })
+                            .success(function () {
+                                set(undefined);
+                            })
+                            .error(function () {
+                                alert.add('Error in logging out.', true);
+                            });
+                    },
+                    save: function (data, callback) {
+                        if (!end) {
+                            defer.resolve();
+                        }
 
-            return {
-                get: function () {
-                    return $rootScope.user;
-                },
-                login: function () {
-                    $window.location.href = '/auth/facebook';
-                },
-                logout: function () {
-                    $http({ method: 'POST', url: '/logout' })
-                        .success(function () {
-                            set(undefined);
-                        })
-                        .error(function () {
-                            alert.add('Error in logging out.', true);
-                        });
-                },
-                save: function (data, callback) {
-                    if (!end) {
-                        defer.resolve();
+                        return $http({method: 'POST', url: '/api/me', timeout: defer.promise, data: data})
+                            .success(function (feedback) {
+                                end = true;
+                                callback(false, feedback);
+                            })
+                            .error(function () {
+                                end = true;
+                                callback(true);
+                            });
                     }
-
-                    return $http({method: 'POST', url: '/api/me', timeout: defer.promise, data: data})
-                        .success(function (feedback) {
-                            end = true;
-                            callback(false, feedback);
-                        })
-                        .error(function () {
-                            end = true;
-                            callback(true);
-                        });
-                }
-            };
-        });
+                };
+            }]);
 }());
