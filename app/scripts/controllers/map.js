d00a3d6ee9b432e37089b365f1661206ade3b906
Henrique Vicente <henriquevicente@gmail.com>
2013-10-21 11:32:50 -0300

    Source formatting / making DI explicit
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -10,157 +10,158 @@
                 templateUrl: 'views/map/place.html'
             };
         })
-        .controller('MapCtrl', function ($scope, $window, $timeout, alert, centers) {
-            var brazilCoordinates = {lat: -10.6500, long: -52.9500},
-                google = $window.google;
+        .controller('MapCtrl', ['$scope', '$window', '$timeout', 'alert', 'centers',
+            function ($scope, $window, $timeout, alert, centers) {
+                var brazilCoordinates = {lat: -10.6500, long: -52.9500},
+                    google = $window.google;
 
-            $scope.mapSettings = {
-                zoom: 3
-            };
+                $scope.mapSettings = {
+                    zoom: 3
+                };
 
-            $scope.newItem = {};
-
-            $scope.limit = 100000;
-
-            $scope.end = true;
-
-            $scope.options = {
-                map: {
-                    center: new google.maps.LatLng(brazilCoordinates.lat, brazilCoordinates.long),
-                    zoom: $scope.mapSettings.zoom,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                },
-                places: {
-                    icon: {
-                        url: '/images/drop-2.png',
-                        scaledSize: new google.maps.Size(15, 20)
-                    }
-                },
-                currentLocation: {
-                    icon: {
-                        url: '/images/current-location-glow.png',
-                        scaledSize: new google.maps.Size(34, 34)
-                    }
-                },
-                selected: {
-                    icon: {
-                        url: '/images/drop-3.png',
-                        scaledSize: new google.maps.Size(15, 20)
-                    }
-                }
-            };
-
-            $scope.places = centers.query();
-
-            $scope.getPlaceOpts = function (place) {
-                return angular.extend(
-                    { title: place.name, animation: google.maps.Animation.DROP },
-                    $scope.options.places
-                );
-            };
-
-            $scope.orderByDistance = function (option) {
-                if (option === 'closest') {
-                    $scope.limit = 5;
-                    $scope.predicate = 'distance';
-                    return;
-                }
+                $scope.newItem = {};
 
                 $scope.limit = 100000;
-                $scope.predicate = '';
-            };
 
-            $scope.selectPlace = function (place, marker) {
-                if ($scope.prev) {
-                    $scope.prev.setOptions($scope.options.places);
-                }
+                $scope.end = true;
 
-                $scope.prev = marker;
+                $scope.options = {
+                    map: {
+                        center: new google.maps.LatLng(brazilCoordinates.lat, brazilCoordinates.long),
+                        zoom: $scope.mapSettings.zoom,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    },
+                    places: {
+                        icon: {
+                            url: '/images/drop-2.png',
+                            scaledSize: new google.maps.Size(15, 20)
+                        }
+                    },
+                    currentLocation: {
+                        icon: {
+                            url: '/images/current-location-glow.png',
+                            scaledSize: new google.maps.Size(34, 34)
+                        }
+                    },
+                    selected: {
+                        icon: {
+                            url: '/images/drop-3.png',
+                            scaledSize: new google.maps.Size(15, 20)
+                        }
+                    }
+                };
 
-                marker.setOptions($scope.options.selected);
+                $scope.places = centers.query();
 
-                marker.setAnimation(google.maps.Animation.BOUNCE);
+                $scope.getPlaceOpts = function (place) {
+                    return angular.extend(
+                        { title: place.name, animation: google.maps.Animation.DROP },
+                        $scope.options.places
+                    );
+                };
 
-                $timeout(function () {
-                    marker.setAnimation(null);
-                }, 1440);
+                $scope.orderByDistance = function (option) {
+                    if (option === 'closest') {
+                        $scope.limit = 5;
+                        $scope.predicate = 'distance';
+                        return;
+                    }
 
-                $scope.place = place;
+                    $scope.limit = 100000;
+                    $scope.predicate = '';
+                };
 
-                $scope.newItem.place = marker.title;
-            };
+                $scope.selectPlace = function (place, marker) {
+                    if ($scope.prev) {
+                        $scope.prev.setOptions($scope.options.places);
+                    }
 
-            $scope.resetPreviousMarker = function () {
-                $scope.prev.setOptions($scope.options.places);
-            };
+                    $scope.prev = marker;
 
-            $scope.setCenter = function (position) {
-                $scope.center = position;
-            };
+                    marker.setOptions($scope.options.selected);
 
-            $scope.getDistance = function (item) {
-                var coords,
-                    userCoords,
-                    p1,
-                    p2,
-                    distance;
-
-                if (!item || !item.location) {
-                    return '';
-                }
-
-                coords = item.location;
-
-                if (!coords.lat || !coords.lng || !$scope.position) {
-                    return '';
-                }
-
-                userCoords = $scope.position.coords;
-
-                p1 = new google.maps.LatLng(coords.lat, coords.lng);
-                p2 = new google.maps.LatLng(userCoords.latitude, userCoords.longitude);
-
-                distance = google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000;
-
-                item.distance = distance;
-
-                if (distance > 100) {
-                    return distance.toFixed(0) + 'km';
-                }
-
-                return distance.toFixed(2) + 'km';
-            };
-
-            $scope.findMe = function () {
-                if (!$window.navigator.geolocation) {
-                    return;
-                }
-
-                $window.navigator.geolocation.getCurrentPosition(function (position) {
-                    var latitude = position.coords.latitude,
-                        longitude = position.coords.longitude;
-
-                    $scope.position = position;
-                    $scope.center = new google.maps.LatLng(latitude, longitude);
-                    $scope.mapSettings.zoom = 12;
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
 
                     $timeout(function () {
-                        $scope.orderByDistance('closest');
-                        $scope.$apply();
-                    }, 1000);
+                        marker.setAnimation(null);
+                    }, 1440);
 
-                    $scope.currentLocationMarker = [{
-                        "name": "Current location",
-                        "location": {
-                            "lat": latitude,
-                            "lng": longitude
-                        }
-                    }];
-                }, function () {
-                    alert.add('Erro ao adquirir geolocalização do usuário.');
-                });
-            };
+                    $scope.place = place;
 
-            $scope.findMe();
-        });
+                    $scope.newItem.place = marker.title;
+                };
+
+                $scope.resetPreviousMarker = function () {
+                    $scope.prev.setOptions($scope.options.places);
+                };
+
+                $scope.setCenter = function (position) {
+                    $scope.center = position;
+                };
+
+                $scope.getDistance = function (item) {
+                    var coords,
+                        userCoords,
+                        p1,
+                        p2,
+                        distance;
+
+                    if (!item || !item.location) {
+                        return '';
+                    }
+
+                    coords = item.location;
+
+                    if (!coords.lat || !coords.lng || !$scope.position) {
+                        return '';
+                    }
+
+                    userCoords = $scope.position.coords;
+
+                    p1 = new google.maps.LatLng(coords.lat, coords.lng);
+                    p2 = new google.maps.LatLng(userCoords.latitude, userCoords.longitude);
+
+                    distance = google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000;
+
+                    item.distance = distance;
+
+                    if (distance > 100) {
+                        return distance.toFixed(0) + 'km';
+                    }
+
+                    return distance.toFixed(2) + 'km';
+                };
+
+                $scope.findMe = function () {
+                    if (!$window.navigator.geolocation) {
+                        return;
+                    }
+
+                    $window.navigator.geolocation.getCurrentPosition(function (position) {
+                        var latitude = position.coords.latitude,
+                            longitude = position.coords.longitude;
+
+                        $scope.position = position;
+                        $scope.center = new google.maps.LatLng(latitude, longitude);
+                        $scope.mapSettings.zoom = 12;
+
+                        $timeout(function () {
+                            $scope.orderByDistance('closest');
+                            $scope.$apply();
+                        }, 1000);
+
+                        $scope.currentLocationMarker = [{
+                            "name": "Current location",
+                            "location": {
+                                "lat": latitude,
+                                "lng": longitude
+                            }
+                        }];
+                    }, function () {
+                        alert.add('Erro ao adquirir geolocalização do usuário.');
+                    });
+                };
+
+                $scope.findMe();
+            }]);
 }());
